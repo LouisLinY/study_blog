c7873815c77890a80ba4c65ed5a58075e7aae740
LouisLinY <louislin15@yahoo.com>
2019-12-29 13:45:55 +0800

    add video server project

---
+++ golang_blog/Actual_Combat_Project/video_server/src/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.POST(")
+	return router
+}
+
+func main() {
+
+
+}
